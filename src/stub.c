9d0dd57e63101fcbe71a4e1535c47eb59953a3a6
shinokaro <shinokaro@hotmail.co.jp>
2024-02-07 05:10:32 +0900

    When unable to retrieve the exit code, set it to indicate failure

--- src/stub.c
+++ src/stub.c
@@ -726,6 +726,7 @@ DWORD CreateAndWaitForProcess(char *ApplicationName, char *CommandLine)
    if (!GetExitCodeProcess(ProcessInformation.hProcess, &exit_code))
    {
       LAST_ERROR("Failed to get exit status");
+      exit_code = -1;
    }
 
    CloseHandle(ProcessInformation.hProcess);
