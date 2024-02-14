f696215ee9d09a733dff8250fde568a89fe97117
Andi <largo@users.noreply.github.com>
2024-02-14 15:11:04 +0900

    Extend tk Sample #10

--- samples/tk.rb
+++ samples/tk.rb
@@ -1,7 +1,11 @@
+# How to use ocran with TK:
+# ocran tk.rb --windows --gem-full=tk --add-all-core --no-autoload
+# You will need to add other gems manually or using the "--gemfile Gemfile" command
+
 require 'tk'
 root = TkRoot.new { title "Hello, World!" }
 TkLabel.new(root) do
    text 'Hello, World!'
    pack { padx 15 ; pady 15; side 'left' }
 end
-Tk.mainloop
+Tk.mainloop unless defined?(Ocran)
