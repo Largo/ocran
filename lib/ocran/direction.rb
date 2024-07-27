eaef4c7eeda13621ae31fe8d13f5b25e204603d8
shinokaro <shinokaro@hotmail.co.jp>
2024-07-27 23:37:00 +0900

    Move all implementations from DirectionTools module to Direction and remove module

--- lib/ocran/direction.rb
+++ lib/ocran/direction.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "direction_tools"
 require_relative "host_config_helper"
 
 module Ocran
@@ -12,13 +11,42 @@ module Ocran
     # - /ruby/vendor_ruby/3.0.0/
     RUBY_LIBRARY_PATH_REGEX = %r{/(ruby/(?:site_ruby/|vendor_ruby/)?\d+\.\d+\.\d+)/?$}i
 
-    include DirectionTools, HostConfigHelper
+    include HostConfigHelper
 
     attr_reader :ruby_executable
 
     def initialize(post_env, pre_env, option)
       @post_env, @pre_env, @option = post_env, pre_env, option
       @ruby_executable = @option.windowed? ? rubyw_exe : ruby_exe
+    end
+
+    # Resolves the common root directory prefix from an array of absolute paths.
+    # This method iterates over each file path, checking if they have a subpath
+    # that matches a given execution prefix.
+    def resolve_root_prefix(files)
+      files.inject(files.first.dirname) do |current_root, file|
+        next current_root if file.subpath?(exec_prefix)
+
+        current_root.ascend.find do |candidate_root|
+          path_from_root = file.relative_path_from(candidate_root)
+        rescue ArgumentError
+          raise "No common directory contains all specified files"
+        else
+          path_from_root.each_filename.first != ".."
+        end
+      end
+    end
+
+    # For RubyInstaller environments supporting Ruby 2.4 and above,
+    # this method checks for the existence of a required manifest file
+    def ruby_builtin_manifest
+      manifest_path = exec_prefix / "bin/ruby_builtin_dlls/ruby_builtin_dlls.manifest"
+      manifest_path.exist? ? manifest_path : nil
+    end
+
+    def detect_dlls
+      require_relative "library_detector"
+      LibraryDetector.loaded_dlls.map { |path| Pathname.new(path).cleanpath }
     end
 
     def to_proc
