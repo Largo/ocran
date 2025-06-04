0345d79e65972f293be96968c8df8577c27b2a79
shinokaro <shinokaro@hotmail.co.jp>
2025-06-05 04:18:09 +0900

    Introduce get_data helper and simplify stream decoding
    - Added static const void *get_data(void **p, size_t size) to advance the data pointer and return the previous location
    - Updated GetLength to use get_data(p, 2) for reading a 2-byte length
    - Refactored GetString to call get_data(p, len) instead of manual pointer arithmetic
    - Changed GetInteger to use get_data(p, 4) and removed explicit pointer adjustments
    - Modified GetOpcode to retrieve a single byte via get_data(p, 1) and return its value
    - Simplified the code in ProcessOpcodes and related routines by replacing manual pointer moves with get_data calls

--- src/unpack.c
+++ src/unpack.c
@@ -10,9 +10,15 @@
 #include <LzmaDec.h>
 #endif
 
+static const void *get_data(void **p, size_t size)
+{
+    const void *data = *p;
+    *p = (char *)(*p) + size;
+    return data;
+}
+
 size_t GetLength(void **p) {
-    unsigned char *b = *p;
-    *p = b + 2;
+    const unsigned char *b = get_data(p, 2);
     return ((size_t)(b[0]) << 0) |
            ((size_t)(b[1]) << 8);
 }
@@ -21,16 +27,13 @@ size_t GetLength(void **p) {
 const char *GetString(void **p)
 {
     size_t len = GetLength(p);
-    const char *str = (const char *)*p;
-    *p = (char *)(*p) + len;
-    return str;
+    return get_data(p, len);
 }
 
 /** Decoder: 32 bit unsigned integer */
 size_t GetInteger(void **p)
 {
-    unsigned char *b = *p;
-    *p = b + 4;
+    const unsigned char *b = get_data(p, 4);
     return ((size_t)(b[0]) <<  0) |
            ((size_t)(b[1]) <<  8) |
            ((size_t)(b[2]) << 16) |
@@ -39,9 +42,8 @@ size_t GetInteger(void **p)
 
 unsigned char GetOpcode(void **p)
 {
-    unsigned char op = *(unsigned char *)*p;
-    *p = (unsigned char *)(*p) + 1;
-    return op;
+    const unsigned char *b = get_data(p, 1);
+    return b[0];
 }
 
 bool ProcessOpcodes(void **p)
@@ -72,8 +74,7 @@ bool ProcessOpcodes(void **p)
             case OP_CREATE_FILE:
                 const char *file_name = GetString(p);
                 size_t file_size = GetInteger(p);
-                const void *data = *p;
-                *p = (char *)(*p) + file_size;
+                const void *data = get_data(p, file_size);
                 if (!file_name || !*file_name) {
                     APP_ERROR("file_name is NULL or empty");
                     return false;
@@ -95,8 +96,7 @@ bool ProcessOpcodes(void **p)
 
             case OP_SET_SCRIPT:            
                 size_t args_size = GetInteger(p);
-                const char *args = *p;
-                *p = (char *)(*p) + args_size;
+                const char *args = get_data(p, args_size);
                 DEBUG("OP_SET_SCRIPT");
                 if (!InitializeScriptInfo(args, args_size)) {
                     return false;
