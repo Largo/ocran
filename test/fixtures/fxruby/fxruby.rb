0e7561634142d5096eef947638d24430ad35bbde
Largo <Largo@users.noreply.github.com>
2026-03-19 01:30:46 +0100

    Change FXRuby Test to run, so it can load socket
    Note that  socket is loaded inside FXRuby at runtime.

--- test/fixtures/fxruby/fxruby.rb
+++ test/fixtures/fxruby/fxruby.rb
@@ -1,10 +1,10 @@
 require 'fox16'
 require 'logger'
 include Fox
-exit if defined?(Ocran)
 logger = Logger.new(STDOUT)
 logger.info("Starting FXRuby test")
 app = FXApp.new
+exit if defined?(Ocran)
 if ARGV[0] == "--test"
     main_window = FXMainWindow.new(app, "Hello World App", :width => 250, :height => 100)
     FXLabel.new(main_window, "Hello, World!")
