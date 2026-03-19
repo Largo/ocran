840ce26c705993ead74223b41415093953de42ae
Largo <Largo@users.noreply.github.com>
2026-03-19 01:03:00 +0100

    Fix Linux/OSX Failure because of missing fox library. Fox test
    Test should fail right now, because socket is not bundled, strange because it is included into ruby itself.

--- test/fixtures/fxruby/fxruby.rb
+++ test/fixtures/fxruby/fxruby.rb
@@ -1,3 +1,15 @@
 require 'fox16'
+require 'logger'
+include Fox
 exit if defined?(Ocran)
+logger = Logger.new(STDOUT)
+logger.info("Starting FXRuby test")
+app = FXApp.new
+if ARGV[0] == "--test"
+    main_window = FXMainWindow.new(app, "Hello World App", :width => 250, :height => 100)
+    FXLabel.new(main_window, "Hello, World!")
+    app.create
+    main_window.show(PLACEMENT_SCREEN)
+    app.run
+end
 exit 0
